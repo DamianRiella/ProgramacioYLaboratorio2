df4e59dad9de6af02987213fb9dcf3861003b3d3
DamianRiella <40808632+DamianRiella@users.noreply.github.com>
2019-03-20 21:46:54 -0300

    Add files via upload

---
+++ Clase1/Clase1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, vali, flag=0;
+
+            do {
+                Console.WriteLine("Ingrese un numero");
+                num = Console.ReadLine();
+
+                if (num > 0)
+                {
+                    int.TryParse(num, out vali);
+                    flag = 1;
+                }
+                else
+                {
+                    Console.WriteLine("Error.¡Reingresar numero!");
+                }
+            } while (flag != 1);
+        }
+    }
+}
---
+++ Clase1/SegundaSolucionClase1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundaSolucionClase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 0, d = 0, e=0;
+            int i, validacion;
+            char opcion;
+
+            Console.WriteLine("Se pedira que ingrese 5 numeros en 5 difernentes casilleros que van del 1 al 5.");
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("señalise casillero(1-5):");
+                opcion= Console.ReadLine();
+                int.TryParse(opcion, out validacion);
+               
+                if (validacion == 1)
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            Console.WriteLine("ingrese un numero:");
+                            a =Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("ingrese un numero:");
+                            b = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("ingrese un numero:");
+                            c = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("ingrese un numero:");
+                            d = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.WriteLine("ingrese un numero:");
+                            e = Console.ReadLine();
+                            break;
+                        default:
+                            Console.WriteLine("Error. Elnumero ingresado no es correcto.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.ReadLine("Error. No se pudo parsear el numero.");
+                }
+               
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
